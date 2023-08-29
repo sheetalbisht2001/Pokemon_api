845ad7cdf3ae73e212882f957cdd08c243b352c3
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-08-29 15:29:48 +0530

    project

---
+++ src/main/java/com/pokemonreview/api/exceptions/ErrorObject.java
@@ -0,0 +1,13 @@
+package com.pokemonreview.api.exceptions;
+
+import lombok.Data;
+import java.util.Date ;
+
+
+@Data
+
+public class ErrorObject {
+    private Integer statusCode;
+    private String message ;
+    private Date timestamp ;
+}
--- src/main/java/com/pokemonreview/api/service/PokemonService.java
+++ src/main/java/com/pokemonreview/api/service/PokemonService.java
@@ -6,6 +6,9 @@ import java.util.List ;
 public interface PokemonService  {
     PokemonDto createPokemon(PokemonDto pokemonDto) ;
     List<PokemonDto> getAllPokemon() ;
+    PokemonDto getPokemonById(int id);
+
+    PokemonDto updatePokemon(PokemonDto pokemonDto, int id) ;
 
 
 
